f436b79c60ce9af23c972c52a890bec0b03c9152
shu yin <shuyin1994916@outlook.com>
2023-11-14 16:26:23 +0800

    20 Add amount unit

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel'
+],  (Controller, JSONModel)=> {
+    'use strict';
+    return Controller.extend("walkthrough.controller.InvoiceList", {
+        onInit(){
+            const oViewModel = new JSONModel({
+                currency :'EUR'
+            });
+            this.getView().setModel(oViewModel,"view")
+        }
+    });
+});
