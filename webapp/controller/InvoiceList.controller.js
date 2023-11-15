78e8ebc24e9189c4cef13baecd1221e07ba6a652
shu yin <shuyin1994916@outlook.com>
2023-11-15 14:19:12 +0800

    define a local function to display the attribute

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,9 +1,14 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/ui/model/json/JSONModel'
-],  (Controller, JSONModel)=> {
+    'sap/ui/model/json/JSONModel',
+    "../model/formatter"
+],  (Controller, JSONModel,formatter)=> {
     'use strict';
     return Controller.extend("walkthrough.controller.InvoiceList", {
+        // object formatter is defined in the model/formatter.js file
+        formatter: formatter,
+        
+        // currency is defined as a global variable in the controller
         onInit(){
             const oViewModel = new JSONModel({
                 currency :'EUR'
