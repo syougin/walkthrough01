ad1e95c48f116ba19f94c4bc244f18427084c2b1
shu yin <shuyin1994916@outlook.com>
2023-11-16 16:42:08 +0800

    mock server

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    "sap/ui/core/util/MockServer",
+    "sap/base/util/UriParameters"
+], (MockServer, UriParameters) => {
+    "use strict";
+
+    return {
+        init:function () {
+            // create
+            const oMockServer = new MockServer({
+                rootUri: sap.ui.require.toUrl("walkthrough") + "/V2/Northwind/Northwind.svc/"
+            });
+
+            const oUriParameters = new UriParameters(window.location.href);
+
+            // configure mock server with a delay
+            MockServer.config({
+                autoRespond: true,
+                autoRespondAfter: oUriParameters.get("serverDelay") || 500
+            });
+
+            // simulate
+            const sPath = sap.ui.require.toUrl("walkthrough/localService");
+            oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
+
+            // start
+            oMockServer.start();
+        }
+    };
+});
